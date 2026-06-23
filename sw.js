1615dff46af81dbf094579e7f5bb7aecf743bf3d
samabrllan-arch <samabrllan@gmail.com>
2026-06-22 19:09:22 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 
-const CACHE_NAME = 'jeico-cache-1782067974466';
+const CACHE_NAME = 'jeico-cache-1782173144037';
 const urlsToCache = [
   './',
   './index.html',
   './estilo1.min.css',
-  './app.min.js?v=1782067974466'
+  './app.min.js?v=1782173144037'
 ];
 
 self.addEventListener('install', event => {
